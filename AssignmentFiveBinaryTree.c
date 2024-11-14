6c1989c4a87724ffd85ccc861bb716bc4a01528d
mkelly674 <114030563+mkelly674@users.noreply.github.com>
2024-11-14 13:26:33 -0500

    Add files via upload

--- AssignmentFiveBinaryTree.c
+++ AssignmentFiveBinaryTree.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Node Node;
 
@@ -26,14 +27,102 @@ Node * createNode(int fine, char * name){
 
 void insertion(Node * root, int fine, char * name){
     if(root == NULL){
-        createNode(fine, name);
+        root = createNode(fine, name);
         return;
     }
     
 }
-int main()
-{
-    printf("Hello World");
+
+int add(Node * root, int fine, char * name){
+
+}
+
+int deduc(Node * root, int deduct, char * name){
+
+}
+
+Node * root(Node * root, char * name){
+
+}
+
+int average(Node * root){
+
+}
+
+int main(){
+    //get the number of commands
+    int numcom = 0;
+    scanf("%d", &numcom);
+
+    //get the command
+    char command[15];
+
+    //get the Node
+    Node * root;
+
+    //get the fine
+    int fin = 0;
+
+    //get the deduction
+    int deduc = 0;
+
+    //get the name
+    char * name;
+
+    for(int i = 0; i < numcom; i++){
+
+        scanf("%s", &command);
+
+        if(strcmp(command, "add") == 0){
+            //get the fine to be added
+            scanf("%d", &fin);
+
+            //get the name to be added
+            scanf("%s", &name);
+
+            root->fine = add(root, fin, name);
+        }
+
+        if(strcmp(command, "deduct") == 0){
+            //get the amount deducted
+            scanf("%d", &deduc);
+
+            //get the name
+            scanf("%s", &name);
+
+
+        }
+
+        if(strcmp(command, "search") == 0){
+            //get the name
+            scanf("%s", &name);
+
+            Node * temp = search(root, name);
+        }
+
+        if(strcmp(command, "average") == 0){
+            //get the average
+            int ave = 0;
+
+            ave = average(root);
+
+            printf("%d \n", ave);
+        }
+
+        if(strcmp(command, "height_balance") == 0){
+            //get the height
+            int hei = 0;
+
+            hei = height_balance(root);
+
+            printf("%d \n", hei);
+        }
+
+        if(strcmp(command, "calc_below") == 0){
+            
+        }
+
+    }
 
     return 0;
 }
