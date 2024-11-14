ade1efcc3fc6c9929865bf4eeedda009615cadf0
mkelly674 <114030563+mkelly674@users.noreply.github.com>
2024-11-14 17:11:25 -0500

    Add files via upload

--- AssignmentFiveBinaryTree.c
+++ AssignmentFiveBinaryTree.c
@@ -17,14 +17,42 @@ struct Node{
 };
 
 
-//prototypes
+//prototypes for nodes
 Node * search(Node*root, char*name);
 Node * createNode(int fine, char * name);
+Node * parent(Node * root, Node * child);
+Node * maxVal(Node * root);
+Node * minVal(Node* root);
+
+//prototypes for void functions
 void insertion(Node * root, int fine, char * name);
 void add(Node * root, int fine, char * name);
 void deduct(Node * root, int deduct, char * name);
+void delete(Node * root, Node * del);
+
+//prototypes for return functions
 int average(Node * root);
 int height_balance(Node * root);
+int calc_below(char * name);
+int isLeaf(Node * root);
+int rightChildOnly(Node * root);
+int leftChildOnly(Node * root);
+
+Node *search(Node * root, char * name){
+    if(root == NULL){
+        return root;
+    }
+    Node * res = root;
+
+    if(strcmp(res->name, name) > 0)
+        res = search(res->Left, name);
+    
+    if(strcmp(res->name, name) < 0)
+        res = search(res->Right, name);
+        
+    
+    return res;
+}
 
 Node * createNode(int fine, char * name){
     Node * temp;
@@ -33,6 +61,46 @@ Node * createNode(int fine, char * name){
     temp->Left = NULL;
     temp->Right = NULL;
     return temp;
+}
+
+Node * parent(Node * root, Node * child){
+    //base case
+    if(root ==NULL)
+        return root;
+
+    //found the parent
+    if(root->Left == child || root->Right == child)
+        return root;
+    
+    //traverse Left
+    if(strcmp(root->name, child->name) > 0)
+        return parent(root->Left, child);
+
+    //traverse Right
+    if(strcmp(root->name, child->name) < 0)
+        return parent(root->Right, child);
+
+    return NULL;
+}
+
+Node * maxVal(Node * root){
+    //finding the max value
+    if(root->Right == NULL)
+        return root;
+
+    //traversing
+    else
+        return maxVal(root->Right);
+}
+
+Node * minVal(Node* root){
+    //finding the min value
+    if(root->Left == NULL)
+        return root;
+
+    //traversing
+    else 
+        return minVal(root->Left);
 }
 
 void insertion(Node * root, int fine, char * name){
@@ -48,7 +116,7 @@ void insertion(Node * root, int fine, char * name){
     if(temp != NULL)
         return;
     
-
+    //traverse the tree
     if(strcmp(root->name, name)> 0)
         insertion(root->Left, fine, name);
     
@@ -69,24 +137,40 @@ void add(Node * root, int fine, char * name){
 }
 
 void deduct(Node * root, int deduct, char * name){
+    if(search(root, name) == NULL)
+        return;
 
+    Node * res = search(root, name);
+    res->fine = res->fine - deduct;
+
+    if(res->fine <= 0)
+    delete(root, res);
 }
 
-Node *search(Node * root, char * name){
-    if(root == NULL){
-        return root;
+void delete(Node * root, Node * del){
+    if(root == NULL)
+    return;
+
+    Node * par = parent(root, del);
+
+    //if root is leaf
+    if(isLeaf(root) == 1){
+        //simple free
+        free(root);
+        root = NULL;
     }
-    Node * res = root;
 
-    if(strcmp(res->name, name) > 0)
-        res = search(res->Left, name);
-    
-    if(strcmp(res->name, name) < 0)
-        res = search(res->Right, name);
+    //if root has only left child
+    if(leftChildOnly(root) == 1){
         
-    
-    return res;
+    }
+
+    //if root has only right child
+    if(rightChildOnly(root) == 1){
+
+    }
 }
+
 
 int average(Node * root){
     int res = 0;
@@ -96,6 +180,25 @@ int average(Node * root){
 
 int height_balance(Node * root){
 
+}
+
+int calc_below(char * name){
+
+}
+
+int isLeaf(Node * root){
+    // returns 1 if leaf node, 0 otherwise
+    return(root->Left == NULL && root->Right == NULL);
+}
+
+int rightChildOnly(Node * root){
+    // returns 1 iff node is right child, 0 otherwise
+    return (root->Left == NULL && root->Right != NULL);
+}
+
+int leftChildOnly(Node * root){
+    // returns 1 iff node is left child, 0 otherwise
+    return (root->Left != NULL && root->Right == NULL);
 }
 
 int main(){
@@ -116,7 +219,7 @@ int main(){
     int deduc = 0;
 
     //get the name
-    char * name;
+    char name[24];
 
     for(int i = 0; i < numcom; i++){
 
@@ -168,7 +271,12 @@ int main(){
         }
 
         if(strcmp(command, "calc_below") == 0){
-            
+            //get the total before the specificed target
+            int tot = 0;
+
+            tot = calc_below(name);
+
+            printf("%d \n", tot);
         }
 
     }
